688dcc4a4eba36e632321b244764ee60a05ea21e
felpsGSantos <felipe.gomes04@hotmail.com>
2023-03-19 14:14:39 -0300

    atualiozando o projeto integrador

--- bkp.js
+++ bkp.js
@@ -1,16 +1,4 @@
-/*
-function aumentaTamanho() {
-    document.getElementById("conteudo").style = "height:600px";
-   
-}
-*/
-/*
-function aparenteElemento() {
 
-    document.getElementById("caixa_pin").style = "display:block";
-    
-}
-*/
 
 function aparenteElemento() {
     /*document.querySelector("form").style = "display:none"*/
@@ -24,3 +12,23 @@ function fecharElemento() {
     document.querySelector(".conteudo").style = "height: 429px;"
 
 }
+
+const input = document.getElementById("telefone");
+
+input.addEventListener("keyup", formatarTelefone);
+
+function formatarTelefone(e){
+
+var v=e.target.value.replace(/\D/g,"");
+
+v=v.replace(/^(\d\d)(\d)/g,"($1)$2"); 
+
+v=v.replace(/(\d{5})(\d)/,"$1-$2");    
+
+e.target.value = v;
+
+ 
+
+}
+
+
